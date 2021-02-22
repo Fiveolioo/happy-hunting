07ba5c572e0483a821162b5edcb46069cf6d07c1
Fiveolioo <96richardohc@gmail.com>
2021-02-21 21:10:38 -1000

    created helper methods, more components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import react from 'react'
 
+
 class App extends React.Component {
-  
+  render() {
+    return(
+      <div>
+        <Router>
+          <div className='App'>
+            <Switch>
+              <Route exact path='/' render={props => <Home{...props}/>} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ frontend/src/components/CarMakes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class CarMakes extends React.Component {
+    render() {
+        return (
+            <DropDown>
+
+            </DropDown>
+        )
+    }
+}
+
+export default CarMakes
